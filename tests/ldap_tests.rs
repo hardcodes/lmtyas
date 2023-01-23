4dbb0c8d6e4784af3ae00faf7cf814038fe3ed69
Sven Putze <github@hardcodes.de>
2023-01-23 15:08:35 +0100

    still need to find out best test organsation/name resolution

--- tests/ldap_tests.rs
+++ tests/ldap_tests.rs
@@ -3,6 +3,8 @@ mod common;
 #[test]
 fn test_ldap_server() {
     let helper_apps: Option<common::ExternalHelperApps> = common::setup();
+    // TODO load config
+    // Path::new(WORKSPACE_DIR).join("conf.dev/lmtyas-config.json"),
     std::thread::sleep(std::time::Duration::from_secs(5));
     assert_eq!(1, 1);
     common::teardown(helper_apps);
