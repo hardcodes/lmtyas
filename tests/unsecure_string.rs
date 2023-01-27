4ea5ea39b9ad760808aa5c26418e19de00c6ae27
Sven Putze <github@hardcodes.de>
2023-01-27 18:12:49 +0100

    added testing the unsecure string trait implementation

---
+++ tests/unsecure_string.rs
@@ -0,0 +1,13 @@
+use lmtyas::unsecure_string::SecureStringToUnsecureString;
+use secstr::SecStr;
+
+#[test]
+fn test_unsecure_string() {
+    const UNSECURE: &str = "unsecure";
+    let secure_string = SecStr::from(UNSECURE);
+    let unsecure_string = secure_string.to_unsecure_string();
+    assert_eq!(
+        unsecure_string, UNSECURE,
+        "strings should match after converting them back from a SecStr!"
+    );
+}
