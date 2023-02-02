9b1d32f51f65fd6c6d7c18d58940ca268569daaa
Sven Putze <github@hardcodes.de>
2023-02-02 15:54:57 +0100

    catch error

--- src/mail_noauth_notls.rs
+++ src/mail_noauth_notls.rs
@@ -1,7 +1,7 @@
 pub use crate::mail_configuration::{SendEMail, SendEMailConfiguration};
-use crate::PROGRAM_NAME;
 use lettre::{Message, SmtpTransport, Transport};
 use std::error::Error;
+use crate::PROGRAM_NAME;
 
 impl SendEMail for SendEMailConfiguration {
     /// This trait is a contract for implementing different
@@ -26,17 +26,23 @@ impl SendEMail for SendEMailConfiguration {
         &self,
         mail_to: &str,
         mail_subject: &str,
-        mail_body: &str,
+        mail_body: &str
     ) -> Result<(), Box<dyn Error>> {
+
         let parsed_mail_from = self.mail_from.parse()?;
         let parsed_mail_to = mail_to.parse()?;
-        let email = Message::builder()
+        let email = match Message::builder()
             .from(parsed_mail_from)
             .to(parsed_mail_to)
             .subject(mail_subject)
             .user_agent(PROGRAM_NAME.to_string())
-            .body(String::from(mail_body))
-            .unwrap();
+            .body(String::from(mail_body)){
+                Ok(m) => m,
+                Err(e) => {return Err(Box::<dyn Error + Send + Sync>::from(format!(
+                    "Error building email message: {}",
+                    e,
+                )));}
+            };
 
         let tp = SmtpTransport::builder_dangerous(&self.mail_server_address);
         let tp_with_port = tp.port(self.mail_server_port);
