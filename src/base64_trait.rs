68dba2c1018705f97bde857d13de6a9fc32ba6cd
Sven Putze <github@hardcodes.de>
2023-02-17 12:25:01 +0100

    Changed result type

--- src/base64_trait.rs
+++ src/base64_trait.rs
@@ -1,21 +1,14 @@
 use std::error::Error;
 
-/// This trait simplifys the use of the base64 endoding and
-/// decoding functions and make them testable so that
-/// future changes induced from the base64 crate can be
-/// validated.
+/// This trait simplifys the use of the base64 string endoding
+/// functions and make them testable so that future changes
+/// induced from the base64 crate can be validated.
 pub trait Base64StringConversions {
     /// convert a string slice to a base64 encoded String.
     fn to_base64_encoded_string(&self) -> String;
 
     /// convert a string slice to a url safe base64 encoded String.
     fn to_base64_urlsafe_encoded_string(&self) -> String;
-
-    /// convert a base64 encoded string slice to a plaintext String.
-    fn from_base64_encoded(b64: &str) -> Result<String, Box<dyn Error>>;
-
-    /// convert a usl safe base64 encoded string slice to a plaintext String.
-    fn from_base64_urlsafe_encoded(b64: &str) -> Result<String, Box<dyn Error>>;
 }
 
 impl Base64StringConversions for String {
@@ -27,16 +20,28 @@ impl Base64StringConversions for String {
         let base64_config = base64::Config::new(base64::CharacterSet::UrlSafe, true);
         base64::encode_config(self.as_bytes(), base64_config)
     }
+}
 
-    fn from_base64_encoded(b64: &str) -> Result<String, Box<dyn Error>> {
-        let bytes = base64::decode(b64)?;
-        let plaintext = String::from_utf8(bytes)?;
-        Ok(plaintext.trim_matches(char::from(0)).to_string())
+/// This trait simplifys the use of the base64 decoding functions
+/// and make them testable so that future changes induced from the
+/// base64 crate can be validated.
+pub trait Base64VecU8Conversions {
+    /// convert a base64 encoded string slice to a plaintext Vec<u8>.
+    fn from_base64_encoded(b64: &str) -> Result<Vec<u8>, Box<dyn Error>>;
+
+    /// convert a usl safe base64 encoded string slice to a plaintext Vec<u8>.
+    fn from_base64_urlsafe_encoded(b64: &str) -> Result<Vec<u8>, Box<dyn Error>>;
+}
+
+impl Base64VecU8Conversions for Vec<u8> {
+    fn from_base64_encoded(b64: &str) -> Result<Vec<u8>, Box<dyn Error>> {
+        Ok(base64::decode(b64)?)
     }
 
-    fn from_base64_urlsafe_encoded(b64: &str) -> Result<String, Box<dyn Error>> {
-        let bytes = base64::decode_config(b64.trim_matches(char::from(0)), base64::URL_SAFE)?;
-        let plaintext = String::from_utf8(bytes)?;
-        Ok(plaintext.trim_matches(char::from(0)).to_string())
+    fn from_base64_urlsafe_encoded(b64: &str) -> Result<Vec<u8>, Box<dyn Error>> {
+        Ok(base64::decode_config(
+            b64.trim_matches(char::from(0)),
+            base64::URL_SAFE,
+        )?)
     }
 }
--- tests/base64_trait.rs
+++ tests/base64_trait.rs
@@ -1,4 +1,4 @@
-use lmtyas::base64_trait::Base64StringConversions;
+use lmtyas::base64_trait::{Base64StringConversions, Base64VecU8Conversions};
 
 const PLAINTEXT: &str = r#"PASS!"§$%&/()=?ß\´`+*~'#-_.:,;<>|WORD"#;
 const B64: &str = r#"UEFTUyEiwqckJSYvKCk9P8OfXMK0YCsqficjLV8uOiw7PD58V09SRA=="#;
@@ -11,31 +11,33 @@ fn test_base64_trait() {
     let base64 = PLAINTEXT.to_string().to_base64_encoded_string();
     assert_eq!(base64, B64, "not the expected base64 encoded value!");
 
-    let plaintext = match String::from_base64_encoded(&base64){
+    let plain_u8 = match Vec::from_base64_encoded(&base64){
         Ok(s) => s,
         Err(e) => {
             panic!("can not decode base64 encoded string slice: {}", &e);
         }
     };
+    let plaintext = String::from_utf8(plain_u8).unwrap();
     assert_eq!(plaintext, PLAINTEXT, "not the expected plaintext");
 
-    if String::from_base64_encoded(NO_BASE64).is_ok(){
+    if Vec::from_base64_encoded(NO_BASE64).is_ok(){
         panic!("should not be able to decode this!");
     }
 
     let base64_urlsafe = PLAINTEXT_URLSAFE.to_string().to_base64_urlsafe_encoded_string();
     assert_eq!(base64_urlsafe, B64_URLSAFE, "not the expected url safe base64 encoded value!");
 
-    let plaintext_urlsafe = match String::from_base64_urlsafe_encoded(&base64_urlsafe){
+    let plain_u8_urlsafe = match Vec::from_base64_urlsafe_encoded(&base64_urlsafe){
         Ok(s) => s,
         Err(e) => {
             panic!("can not decode url safe base64 encoded string slice: {}", &e);
         }
     };
 
+    let plaintext_urlsafe = String::from_utf8(plain_u8_urlsafe).unwrap();
     assert_eq!(plaintext_urlsafe, PLAINTEXT_URLSAFE, "not the expected url safe plaintext");
 
-    if String::from_base64_urlsafe_encoded(NO_BASE64).is_ok(){
+    if Vec::from_base64_urlsafe_encoded(NO_BASE64).is_ok(){
         panic!("should not be able to decode this!");
     }
 }
