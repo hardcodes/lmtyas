f857fc62701fdd419648bc25870a961b6f86e833
Sven Putze <github@hardcodes.de>
2023-02-27 15:35:59 +0100

    disable tell button when system is not ready

--- static/js/index.js
+++ static/js/index.js
@@ -1,1 +1,7 @@
-queryWebService("/system/is_server_ready", validateSystemStatus, systemIsNotReady);
+const tellButton = document.getElementById("tell");
+queryWebService("/system/is_server_ready", validateSystemStatus, notReady);
+
+function notReady(){
+    tellButton.disabled = true;
+    systemIsNotReady;
+}
