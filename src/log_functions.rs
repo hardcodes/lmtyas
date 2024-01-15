ee144784970cf7ec3577ebc6320b36ea2df6373e
Sven Putze <github@hardcodes.de>
2024-01-15 15:47:22 +0100

    addded testing of edge cases

--- src/log_functions.rs
+++ src/log_functions.rs
@@ -1,5 +1,7 @@
 /// strip of parameters from the request uri/path
-/// so that it does not appear in the log file.
+/// so that it does not appear in the log file e.g.
+/// `https://my.server.tld/route/to/hell?param=value` is stripped down to
+/// `https://my.server.tld/route/to/hell`
 #[inline]
 pub fn extract_request_path(path: &str) -> String {
     // no parameters in path
--- tests/log_functions.rs
+++ tests/log_functions.rs
@@ -2,7 +2,11 @@ use lmtyas::log_functions::extract_request_path;
 
 #[test]
 fn extract_request_path_test() {
-    assert_eq!(extract_request_path(r#"/test"#), "/test");
-    assert_eq!(extract_request_path(r#"/before?"#), "/before");
-    assert_eq!(extract_request_path(r#"/before?after="#), "/before");
-}
+    assert_eq!(extract_request_path(r"/test"), "/test");
+    assert_eq!(extract_request_path(r"/before?"), "/before");
+    assert_eq!(extract_request_path(r"/before?after="), "/before");
+    assert_eq!(extract_request_path(r"/before???"), "/before");
+    assert_eq!(extract_request_path(r"/before?after=value?what=happend"), "/before");
+    assert_eq!(extract_request_path(r"https://my.server.tld/route/to/hell?after=value?what=happend"), r"https://my.server.tld/route/to/hell");
+    assert_eq!(extract_request_path(r"https://my.server.tld/route/to/hell???after=value?what=happend"), r"https://my.server.tld/route/to/hell");
+}   
